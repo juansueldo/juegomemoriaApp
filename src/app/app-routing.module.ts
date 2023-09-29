c5726094d488c23dce8dea846ca9e04164e022cf
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2023-09-29 14:58:39 +0000

    paginas añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'ranking',
+    loadChildren: () => import('./pages/ranking/ranking.module').then( m => m.RankingPageModule)
+  },
 ];
 
 @NgModule({
